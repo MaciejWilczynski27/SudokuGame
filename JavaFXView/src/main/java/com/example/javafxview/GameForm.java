ad669b03f589bd0bff521975a91d958eb2fdf021
242546 <242546@edu.p.lodz.pl>
2022-12-15 22:07:39 +0100

    Dodanie języków i zapis planszy

--- JavaFXView/src/main/java/com/example/javafxview/GameForm.java
+++ JavaFXView/src/main/java/com/example/javafxview/GameForm.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -17,31 +22,35 @@ import javafx.scene.text.TextAlignment;
 import javafx.scene.control.TextArea;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-import org.example.BacktrackingSudokuSolver;
-import org.example.SudokuBoard;
-import org.example.SudokuSolver;
+import org.example.*;
 
 
 public class GameForm implements Initializable {
 
-    public GridPane obszarSudoku = new GridPane();
+     @FXML
+     public GridPane obszarSudoku;
+     @FXML
+    public Button bzapiszgre;
+     @FXML
+    public Button bsprawdz;
 
+    SudokuSolver solver = new BacktrackingSudokuSolver();
+    SudokuBoard board = new SudokuBoard(solver);
+    SudokuBoard playerBoard;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
+
+
     public void printBoard(Level level) throws IOException {
-        SudokuSolver solver = new BacktrackingSudokuSolver();
-        SudokuBoard board = new SudokuBoard(solver);
+
         board.solveGame();
-        SudokuBoard playerBoard = (SudokuBoard) board.clone();
+        playerBoard = (SudokuBoard) board.clone();
         level.removeFields(playerBoard);
         TextArea textArea = new TextArea();
-        RowConstraints rc = new RowConstraints();
-        ColumnConstraints cc = new ColumnConstraints();
-
         for(int x = 0;x < 9;x++) {
             for(int y = 0;y < 9;y++) {
                 if (playerBoard.getBoard(x,y).getFieldValue()!=0) {
@@ -55,22 +64,45 @@ public class GameForm implements Initializable {
                     textArea.setFont(Font.font(14));
                     obszarSudoku.add(textArea1, x, y);
                 }
-                rc.setPrefHeight(50);
-                cc.setPrefWidth(50);
-                obszarSudoku.getRowConstraints().add(rc);
-                obszarSudoku.getColumnConstraints().add(cc);
+            }
+        }
+    }
+
+    public void zapiszGre() throws IOException, ClassNotFoundException {
+        SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
+        Dao<SudokuBoard> fileSudokuBoardDao;
+        fileSudokuBoardDao = factory.getFileDao("zapis");
+
+        for(int i = 0;i < 9;i++) {
+            for(int j = 0;j < 9;j++) {
+               TextArea tmp = (TextArea) getNodeByRowColumnIndex(i,j,obszarSudoku);
+               if(tmp.getText() != "") {
+                   playerBoard.setBoard(i, j, Integer.valueOf(tmp.getText()));
+               }
+               else {
+                   playerBoard.setBoard(i, j, 0);
+               }
+            }
+        }
+        fileSudokuBoardDao.write(playerBoard);
+    }
+
+    public void sprawdzPlansze(ActionEvent actionEvent) {
+
+
+    }
+
+    public Node getNodeByRowColumnIndex (final int row, final int column,GridPane gridPane) {
+        Node result = null;
+        ObservableList<Node> childrens = gridPane.getChildren();
+
+        for (Node node : childrens) {
+            if(gridPane.getRowIndex(node) == row && gridPane.getColumnIndex(node) == column) {
+                result = node;
+                break;
             }
         }
 
-        Stage stage = new Stage();
-        Scene scene = new Scene(obszarSudoku, 700, 700);
-        stage.setTitle("Gra");
-        stage.setScene(scene);
-        obszarSudoku.setLayoutX(150);
-        obszarSudoku.setLayoutY(150);
-
-
-        stage.show();
+        return result;
     }
-
 }
--- JavaFXView/src/main/java/com/example/javafxview/Menu.java
+++ JavaFXView/src/main/java/com/example/javafxview/Menu.java
@@ -1,20 +1,43 @@
 package com.example.javafxview;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Menu {
+    private ResourceBundle resourceBundle = ResourceBundle.getBundle("bundle_pl_PL");
+
+    public ResourceBundle getResourceBundle() {
+        return resourceBundle;
+    }
+
+    public void setResourceBundle(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
     @FXML
     public Button bpoziom3;
     @FXML
     public Button bpoziom1;
     @FXML
     public Button bpoziom2;
+
+    @FXML
+    public Button eng;
+
+    @FXML
+    public Button pl;
+
+    @FXML
+    public Text langchoose;
 
     static
     GameForm gameForm = null;
@@ -29,6 +52,20 @@ public class Menu {
 
     public void setPoziom3() throws IOException {
         openForm(Level.HARD);
+    }
+    @FXML
+    public void languageChange(String language) {
+        setResourceBundle(ResourceBundle.getBundle("bundle_" + language));
+        bpoziom1.setText(getResourceBundle().getString("Level") + " 1");
+        bpoziom2.setText(getResourceBundle().getString("Level") + " 2");
+        bpoziom3.setText(getResourceBundle().getString("Level") + " 3");
+        langchoose.setText(getResourceBundle().getString("langchoose"));
+    }
+    public void setEnglish() throws IOException {
+        languageChange("en_EN");
+    }
+    public void setPolish() throws IOException {
+        languageChange("pl_PL");
     }
 
     public void showStage() throws IOException {
@@ -51,7 +88,7 @@ public class Menu {
         stage.setScene(scene);
         gameForm = loader.getController();
         gameForm.printBoard(level);
-
+        stage.show();
 
     }
 
